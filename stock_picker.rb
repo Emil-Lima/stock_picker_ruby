e1d77f341d186ca81871d8be65d82f1d6596bb27
J-Rozas <jrozaslima@gmail.com>
2021-01-24 21:42:26 +0000

    Refactored code and fixed a bug that made the program return a faulty result.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,24 +1,23 @@
-def stock_picker(arr)
-    arr_copy = arr
-    value_holder = 0
-    best_days = [0,0]
-    arr.map do |element|
-        arr_copy.map do |second_element|
-            if arr.index(element) < arr_copy.index(second_element)
-                if (element < second_element)
-                    if (second_element - element > value_holder)
-                        value_holder = second_element - element
-                        best_days.pop(2)
-                        best_days.push(arr.index(element), arr_copy.index(second_element))
-                    end
-                end
-            end
-        end
-    end
-    if best_days == [0,0]
-        return "Do noy buyyyyyyyy!!"
-    end
-    best_days
-end
+# frozen_string_literal: true
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+def stock_picker(arr)
+  value_holder = 0
+  best_days = [0, 0]
+  last_element = arr.length - 1
+  while value_holder < last_element
+    if arr[value_holder + 1..last_element].max - arr[value_holder] > best_days[1] - best_days[0]
+      best_days = [arr[value_holder], arr[value_holder + 1..last_element].max]
+    end
+    value_holder += 1
+  end
+  return 'No good options' if best_days == [0, 0]
+
+  final = []
+  arr.each_with_index do |ele, index|
+    if ele == best_days[0]
+      final.push(index)
+      best_days.shift
+    end
+  end
+  final
+end
